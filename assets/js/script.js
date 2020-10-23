cb849dabf7804b33c55b3198f4d740765ca65091
Gary Hughes <gary.hughes87@virginmedia.com>
2020-10-23 11:38:58 +0000

    Added shuffle newDeck function to replace jquery snippet

--- assets/js/script.js
+++ assets/js/script.js
@@ -203,6 +203,7 @@ function startGame() {
 
   // Create a new array by merging two copies of the new shuffDeck together
   newDeck = shuffDeck.concat(shuffDeck);
+  shuffle(newDeck);
   console.log(newDeck);
 
   // Repeat over cardDeck array
@@ -223,28 +224,6 @@ function startGame() {
     });
     deck.appendChild(liEl);
   }
-  newDeck.sort(shuffle);
-  (function ($) {
-    $.fn.shuffle = function () {
-      var allElems = this.get(),
-        getRandom = function (max) {
-          return Math.floor(Math.random() * max);
-        },
-        shuffled = $.map(allElems, function () {
-          var random = getRandom(allElems.length),
-            randEl = $(allElems[random]).clone(true)[0];
-          allElems.splice(random, 1);
-          return randEl;
-        });
-
-      this.each(function (i) {
-        $(this).replaceWith($(shuffled[i]));
-      });
-
-      return $(shuffled);
-    };
-  })(jQuery);
-  $("li img").shuffle();
 }
 if (currentCards < cardLimit) {
   currentCards++;
